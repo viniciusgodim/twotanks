365402a3f1fa53b0998a188ff702124bd3e7696a
Vinícius de Souza Godim de Oliveira <33293145+viniciusgodim@users.noreply.github.com>
2021-04-23 23:02:31 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {useState,useEffect,useRef} from 'react';
-import Slider from '@material-ui/core/Slider';
 import DiscreteSlider from './components/DiscreteSlider'
 import useInterval from './components/UseInterval'
 import W from 'lambert-w-function'
@@ -90,14 +89,15 @@ function Layout({leftA,topA,diameterA,heightA,heightDiff,length,diameterB,height
   )
 }
 
+
 function App() {
-  let [diameterA,setDiameterA] = useState(200);
-  let [diameterB,setDiameterB] = useState(200);
-  let [length,setLength] = useState(750);
+  let [diameterA,setDiameterA] = useState(0);
+  let [diameterB,setDiameterB] = useState(0);
+  let [length,setLength] = useState(0);
   let [liquidHeightA,setLiquidHeightA] = useState(150);
   let [liquidHeightB,setLiquidHeightB] = useState(0);
-  let [heightDiff,setHeightDiff] = useState(50);
-  let [pipeDiameter,setPipeDiameter] = useState(1.5);
+  let [heightDiff,setHeightDiff] = useState(0);
+  let [pipeDiameter,setPipeDiameter] = useState(0);
   const pxToMeter = 1/100
   const viscosity = 1e-3;
   const density = 1e3;
@@ -110,8 +110,8 @@ function App() {
   let liquidHeightBMeters = liquidHeightB*pxToMeter;
   let heightDiffMeters = heightDiff*pxToMeter;
   let lengthMeters = length*pxToMeter
-  let velocity = 1;
-  let velocityNew = 2;
+  let velocity = Math.sqrt(2*gravity*liquidHeightAMeters);
+  let velocityNew = 2*velocity;
   let dv = 1;
 
   useInterval(() => {
@@ -121,27 +121,31 @@ function App() {
       if (reynolds < 2100) {
         frictionFactor = 64/reynolds
       } else {
-        frictionFactor = 1/((-2*Math.log10(10^(-1/Math.log(10)*W(reynolds*Math.log(10)/5.02))))**2)
+        frictionFactor = 1/((-2*Math.log10(10**(-1/Math.log(10)*W(reynolds*Math.log(10)/5.02))))**2)
       }
-      let headLoss = frictionFactor*velocity^2/(2*gravity)*lengthMeters/pipeDiameterMeters
-      velocityNew = Math.sqrt(2*gravity*(headLoss-liquidHeightAMeters-heightDiffMeters)/((pipeDiameterMeters/diameterAMeters)^2-1))
+      let headLoss = frictionFactor*velocity**2/(2*gravity)*lengthMeters/pipeDiameterMeters
+      if (liquidHeightAMeters+heightDiffMeters>headLoss){
+        velocityNew = Math.sqrt(2*gravity*(liquidHeightAMeters+heightDiffMeters-headLoss)/(1-(pipeDiameterMeters/diameterAMeters)**4))
+      } else{
+        velocityNew = velocity/2
+      }
       dv = velocity - velocityNew;
       velocity = velocityNew
     }
     let dh = dt*velocity*(pipeDiameterMeters/diameterAMeters)**2
     liquidHeightAMeters -= dh;
-    liquidHeightBMeters += dh;
+    liquidHeightBMeters += dh*(diameterAMeters/diameterBMeters)**2;
     setLiquidHeightA(liquidHeightAMeters/pxToMeter);
     setLiquidHeightB(liquidHeightBMeters/pxToMeter);
   }, 1000);
 
   return (
     <>
-    <DiscreteSlider setSize={setDiameterA} size={diameterA} defaultValue="200" text="Draining tak diameter" min="50" max="500" step="10"/>
-    <DiscreteSlider setSize={setDiameterB} size={diameterB} defaultValue="200" text="Filling tak diameter" min="50" max="500" step="10"/>
-    <DiscreteSlider setSize={setLength} size={length} defaultValue="500" text="Pipe length" min="50" max="750" step="10"/>
-    <DiscreteSlider setSize={setHeightDiff} size={heightDiff} defaultValue="50" text="Height difference" min="0" max="500" step="10"/>
-    <DiscreteSlider setSize={setPipeDiameter} size={pipeDiameter} defaultValue="1.5" text="Pipe diameter" min="0.1" max="5" step="0.1"/>
+    <DiscreteSlider setSize={setDiameterA} size={diameterA} defaultValue="250" text="Draining tak diameter" min="50" max="500" step="10"/>
+    <DiscreteSlider setSize={setDiameterB} size={diameterB} defaultValue="250" text="Filling tak diameter" min="50" max="500" step="10"/>
+    <DiscreteSlider setSize={setLength} size={length} defaultValue="750" text="Pipe length" min="50" max="750" step="10"/>
+    <DiscreteSlider setSize={setHeightDiff} size={heightDiff} defaultValue="200" text="Height difference" min="0" max="500" step="10"/>
+    <DiscreteSlider setSize={setPipeDiameter} size={pipeDiameter} defaultValue="2.5" text="Pipe diameter" min="0.1" max="5" step="0.1"/>
 
 
     <Layout leftA="350px" topA="100px"
